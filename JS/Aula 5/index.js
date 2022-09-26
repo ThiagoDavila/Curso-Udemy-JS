cd8dc49407ae9ffebe1d840d0b2a853e59cb3f57
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-09-26 14:17:46 -0300

    Create index.js

---
+++ JS/Aula 5/index.js
@@ -0,0 +1,17 @@
+const nome = 'Luiz' //string
+const nome1 = "Luiz" //string
+const nome2 = `Luiz` //string
+const num1 = 10 //number
+const num2 = 10.52 //number
+let nomeAluno // undefined -> não aponta pra local nenhum na memória
+let sobrenomeAluno = null //Nulo -> não aponta pra lolca nenhum na memoria 
+const aprovado = false //boolean = true, false (lógico)
+
+//console.log(typeof aprovado) para ver o tipo
+
+const a = 2
+const b = a 
+console.log(a, b)//2, 2
+
+a = 3
+console.log(a, b) //3, 2
