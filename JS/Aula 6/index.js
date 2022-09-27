d70830bf8c52efc2e878f30cd26e4d30b7362174
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-09-27 19:35:51 -0300

    Create index.js

---
+++ JS/Aula 6/index.js
@@ -0,0 +1,15 @@
+/*let contador = 1
+contador++ //2
+contador++ //3
+contador++ //4
+contador++ //5
+console.log(contador)
+*/
+
+/*
+Incremento = ++
+Decremento = --
+*/ 
+
+let contador = 0
+contador += 2 //contador = contador + 50
