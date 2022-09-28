43670001c0bd8fbbf2c197dd9a7573ca36a105c0
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-09-28 11:59:15 -0300

    creat aula 7

---
+++ JS/Aula 7/js/script.js
@@ -0,0 +1,14 @@
+/*alert('Com a nossa msg')
+window.confirm('Deseja realmente apagar?')
+window.prompt('Digite o seu nome:')
+*/
+
+let num1=window.prompt('Digite um Número:')
+let num2=window.prompt('Digite outro Número:')
+num1 = Number(num1)
+num2 = Number(num2)
+const resultado = num1 + num2
+
+alert(`O soma dos número é: ${resultado}`)
+
+
