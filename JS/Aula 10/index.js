d01a09701054ccdd6f61fc7c1e764686283bdaa4
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-10-05 17:49:01 -0300

    Create index.js

---
+++ JS/Aula 10/index.js
@@ -0,0 +1,12 @@
+let num1 = 10
+let num2 = 2.5
+
+num1 += num2 //num1 + num1 + num2
+
+//console.log(num1.toString() + num2)
+//num1 = num1.toString()
+//console.log(num1.toString(2) + num2)
+//console.log(num1.toFixed(2) + num2)
+//console.log(Number.isIntenger(num1))
+//let temp = num1 + 'ola'
+//console.log(Number.isNaN(temp))
