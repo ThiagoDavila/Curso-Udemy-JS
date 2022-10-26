6a4e71d7d6a70ff24541e83163a9ce0ba09f22c5
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-10-26 17:56:33 -0300

    att

--- JS/Aula 17/assets/js/main.js
+++ JS/Aula 17/assets/js/main.js
@@ -3,15 +3,26 @@ const form = document.querySelector('#form')
 
 form.addEventListener('submit', function(event){
     event.preventDefault()
-    console.log('Evento previnido')
+    const inputPeso = event.target.querySelector('#peso')
+    const inputAltura = event.target.querySelector('#altura')
+
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    console.log(peso, altura)
 
 })
-function setResultado (msg){
+
+function criaP(){
+    const p = document.createElement('p')
+    return p
+}
+
+
+    function setResultado (msg){
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = ''
-    const p = document.createElement('p')
-    p.innerHTML = 'qualquer coisa'
-    resultado.appendChild(p)
+    const p = criaP()
 
 }
 
