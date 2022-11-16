78ca28c5a8b5bb3fe78da2374c38d06514374ff3
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-11-16 17:03:50 -0300

    Create index.js

---
+++ JS/Aula 18/index.js
@@ -0,0 +1,11 @@
+const pontuacaoUsuario = 999
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario Normal'
+console.log(nivelUsuario)
+
+
+
+//if (pontuacaoUsuario >=1000){
+//    console.log('Usuario VIP')
+//} else {
+//   console.log('Usuario Normal')
+//}
