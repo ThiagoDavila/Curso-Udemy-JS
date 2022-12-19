1e2c6860f6d2235d20d128fa7854acfd8a381544
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2022-12-19 12:01:31 -0300

    Update index.js

--- JS/Aula 22/index.js
+++ JS/Aula 22/index.js
@@ -5,6 +5,20 @@
 
 //                   1        2        3
 //                0  1  2   0  1  2   0  1  2 
-const numeros = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+//const numeros = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
 
-console.log(numeros[1][2])
+//console.log(numeros[1][2])
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 30,
+    endereco: {
+        rua: 'Av Brasil',
+        numero: 320
+    }
+}
+
+// Atribuição via desestruração
+const {nome, sobrenome, endereco:{rua,numero}} = pessoa
+console.log(nome, sobrenome, rua)
