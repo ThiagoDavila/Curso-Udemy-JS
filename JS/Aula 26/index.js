18c0d8143e153504bcfebd1d151a5cf6573a0ef4
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2023-06-01 16:57:44 -0300

    aula 27

--- JS/Aula 26/index.js
+++ JS/Aula 26/index.js
@@ -3,6 +3,7 @@ const nome = ['Thiago', 'Davila', 'Valejo']
 for (let valor of nome){
     console.log(valor)
 }
+
 nome.forEach(function(valor, indice){
-    console.log(valor, indice);
+    console.log(valor);
 });
