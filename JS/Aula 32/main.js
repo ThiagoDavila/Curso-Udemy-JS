b577afb53164686ef75356155ccb16ebd9a3d7b3
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2023-07-03 11:56:23 -0300

    att

--- JS/Aula 32/main.js
+++ JS/Aula 32/main.js
@@ -4,8 +4,21 @@ function mostraHora(){
     return data.toLocaleTimeString('pt-BR', {hour12: false})
 }
 
-function funcaoDoInterval(){
+/*function funcaoDoInterval(){
     console.log(mostraHora)
-}
+}*/ 
+//não precisa criar essa função basta fazer essa aqui de baixo
 
-setInterval()
+const timer = setInterval(function(){
+    console.log(mostraHora())
+}, 1000)
+
+
+//serve para parar o código o numero 10000(dez mil é a quantidade de milisegundos) ou seja daqui a 10s vai parar
+setTimeout(function(){
+    clearInterval(timer)
+}, 3000)
+
+setTimeout(function(){
+    console.log('olá mundo!')
+}, 3000)
---
+++ JS/Aula 32/node_fe7709886e458.tmp.js
@@ -0,0 +1,14 @@
+function mostraHora(){
+    let data = new Date()
+    
+    return data.toLocaleTimeString('pt-BR', {hour12: false})
+}
+
+/*function funcaoDoInterval(){
+    console.log(mostraHora)
+}*/ 
+//não precisa criar essa função basta fazer essa aqui de baixo
+
+setInterval(function(){
+    console.log(mostraHora)
+}, 1000)
