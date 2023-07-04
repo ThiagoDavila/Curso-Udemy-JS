95b2436dfd745d215c1c76cec9d42641d8d15514
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2023-07-04 12:19:07 -0300

    att

---
+++ JS/Aula 33/js/main.js
@@ -0,0 +1,17 @@
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+
+iniciar.addEventListener('click', function(event){
+    alert('Cliquei no inciar')
+})
+
+pausar.addEventListener('click', function(event){
+    alert('Cliquei no pausar')
+})
+
+zerar.addEventListener('click', function(event){
+    alert('Cliquei no zerar')
+})
+
