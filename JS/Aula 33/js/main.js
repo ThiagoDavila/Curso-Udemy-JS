00d06f382a0b48f34db32713b077dbaf9028ef22
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2023-07-04 17:04:59 -0300

    relogio

--- JS/Aula 33/js/main.js
+++ JS/Aula 33/js/main.js
@@ -1,17 +1,56 @@
-const relogio = document.querySelector('.relogio')
-const iniciar = document.querySelector('.iniciar')
-const pausar = document.querySelector('.pausar')
-const zerar = document.querySelector('.zerar')
+function relogio(){
+function getTimeFromSeconds(segundos){
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {hour12: false,
+    timeZone: 'GMT'
+    })
+}
 
-iniciar.addEventListener('click', function(event){
-    alert('Cliquei no inciar')
+const relogio = document.querySelector('.relogio')
+//const iniciar = document.querySelector('.iniciar')
+//const pausar = document.querySelector('.pausar')
+//const zerar = document.querySelector('.zerar')
+let segundos = 0
+let timer
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos++
+        relogio.innerHTML = getTimeFromSeconds(segundos)
+    }, 1000)
+}
+
+document.addEventListener('click',function(e){
+    const el = e.target
+   
+    if(el.classList.contains('iniciar')){
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        iniciaRelogio()
+    }
+    if(el.classList.contains('pausar')){
+        relogio.classList.add('pausado')   
+        clearInterval(timer)
+    }
+    if(el.classList.contains('zerar')){
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00'
+        segundos = 0
+    }
+})
+}
+relogio()
+
+/*iniciar.addEventListener('click', function(event){
+   
 })
 
 pausar.addEventListener('click', function(event){
-    alert('Cliquei no pausar')
+   
 })
 
 zerar.addEventListener('click', function(event){
-    alert('Cliquei no zerar')
+  
 })
-
+*/
